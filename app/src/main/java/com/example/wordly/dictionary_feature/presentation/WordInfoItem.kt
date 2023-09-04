5900d0c6bd73ae963f769db01dbc76e529c57711
YadavYashvant <yashvantyadav855@gmail.com>
2023-09-04 12:22:34 +0530

    changed dependencies' version

--- app/src/main/java/com/example/wordly/dictionary_feature/presentation/WordInfoItem.kt
+++ app/src/main/java/com/example/wordly/dictionary_feature/presentation/WordInfoItem.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.wordly.dictionary_feature.domain.model.WordInfo
