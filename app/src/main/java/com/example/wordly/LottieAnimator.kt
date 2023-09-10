5da507a8162fc4057bef59d99f4a19f72ea865ca
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-11 00:47:29 +0530

    adding an animation

--- app/src/main/java/com/example/wordly/LottieAnimator.kt
+++ app/src/main/java/com/example/wordly/LottieAnimator.kt
@@ -1,8 +1,17 @@
 package com.example.wordly
 
 import androidx.compose.runtime.Composable
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
 
 @Composable
+@Preview
 fun LottieAnimator() {
+
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.search))
+    LottieAnimation(composition = composition)
     //not in use till now!
 }
