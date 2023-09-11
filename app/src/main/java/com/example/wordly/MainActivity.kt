a949364d1b3acf8c2c0c7c0ffc4128e8e1f7bcbf
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-12 00:51:39 +0530

    adding util icons

--- app/src/main/java/com/example/wordly/MainActivity.kt
+++ app/src/main/java/com/example/wordly/MainActivity.kt
@@ -115,6 +115,8 @@ fun Navigation() {
                                 message = event.message
                             )
                         }
+
+                        else -> {}
                     }
                 }
             }
