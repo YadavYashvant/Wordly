8a23f6d35640b74090986825f8e171b844586bd4
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-12 00:57:47 +0530

    adding util icons

--- app/src/main/java/com/example/wordly/Util_Icons.kt
+++ app/src/main/java/com/example/wordly/Util_Icons.kt
@@ -1,2 +1,14 @@
 package com.example.wordly
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExitAppIcon() {
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Outlined.ExitToApp, contentDescription = null)
+    }
+}
